0828dbf1c0d8c54277065559b692431b187f32d3
Mark Hess <mark.hess0@gmail.com>
2021-01-12 14:48:04 +1100

    Bump version: 1.0.1 → 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="notify_me",
-    version="1.0.1",
+    version="1.1.0",
     author="Mark Hess",
     author_email="author@example.com",
     description="Get notified on discord when a topic of interest triggers",
