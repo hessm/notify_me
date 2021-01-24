9908107ea95db6342e65f0650db7c9bf0f5e873c
Mark Hess <mark.hess0@gmail.com>
2021-01-24 14:25:47 +1100

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="notify_me",
-    version="2.0.0",
+    version="2.1.0",
     author="Mark Hess",
     description="Get notified on discord when a topic of interest triggers",
     long_description=long_description,
